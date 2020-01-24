98dad79a7997d955b985a7f583c46aaf78f1934d
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-24 20:47:03 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,18 @@
 module Memorable
   module ClassMethods
     def reset_all
-      all.clear
+      self.all.clear
     end
   
     def count
-      all.count
+      self.all.count
     end
   end
   
   module InstanceMethods
   
     def initialize
-      
+      self.class.all << self
     end
   end
   
